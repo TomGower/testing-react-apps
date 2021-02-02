c78904ef5b43b4afefcd721e069d9c1fce075da2
TomGower <tom.gower@gmail.com>
2021-02-02 17:54:59 -0600

    Solved exercise 04 extra credit 4

--- src/__tests__/exercise/04ec4.js
+++ src/__tests__/exercise/04ec4.js
@@ -4,6 +4,7 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+// import faker from 'faker'
 import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
@@ -15,12 +16,12 @@ test('submitting the form calls onSubmit with username and password', () => {
   const password = screen.getByLabelText(/password/i)
   const submit = screen.getByRole('button', {name: /submit/i})
 
-const buildLoginForm = build({
-  fields: {
-    username: fake(f => f.internet.userName()),
-    password: fake(f => f.internet.password()),
-  },
-})
+  const buildLoginForm = build({
+    fields: {
+      testUsername: fake(f => f.internet.userName()),
+      testPassword: fake(f => f.internet.password()),
+    },
+  })
   const {testUsername, testPassword} = buildLoginForm({testPassword: 'abc'})
 
   userEvent.type(username, testUsername)
