a4d2088ab62dd17415320e4044fe21565d6622d1
TomGower <tom.gower@gmail.com>
2021-02-04 14:44:25 -0600

    Solution to exercise 06 extra credit 1

--- src/__tests__/exercise/06ec1.js
+++ src/__tests__/exercise/06ec1.js
@@ -6,6 +6,7 @@ import {render, screen, act} from '@testing-library/react'
 import {useCurrentPosition} from 'react-use-geolocation'
 import Location from '../../examples/location'
 
+// this re-writes the module to be a jest mock function
 jest.mock('react-use-geolocation')
 
 test('displays the users current location', async () => {
@@ -17,7 +18,9 @@ test('displays the users current location', async () => {
   }
   
   let setReturnValue
+  // custom hook for testing mock
   function useMockCurrentPosition() {
+    // useCurrentPosition in react-use-geolocation returns an array, thus the array here
     const state = React.useState([])
     setReturnValue = state[1]
     return state[0]
@@ -25,8 +28,13 @@ test('displays the users current location', async () => {
   useCurrentPosition.mockImplementation(useMockCurrentPosition)
 
   render(<Location />)
+  // if the thing we are mocking has arguments, we would want to check here if it had been called with correct arguments
+  // expect(useCurrentPosition).toHaveBeenCalledWith('all the greatest arguments')
+  // but we don't want to also mock how many times it's been called, because React will do its own thing,
+  // function components could be idempotent (doesn't matter how many times they've been called)
   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
 
+  // do some action, then flush all the side effects
   act(() => {
     setReturnValue([fakePosition])
   })
